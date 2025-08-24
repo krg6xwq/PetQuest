e46914245502a8d4de5179e5aa5b6635b30365aa
Kurt Gorg <kurtgorg@gmail.com>
2025-08-24 17:32:07 +0800

    Initial Commit

---
+++ PetQuest/PetQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetQuestApp.swift
+//  PetQuest
+//
+//  Created by kurt on 24/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PetQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetQuestTests/PetQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetQuestTests.swift
+//  PetQuestTests
+//
+//  Created by kurt on 24/8/25.
+//
+
+import Testing
+@testable import PetQuest
+
+struct PetQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
